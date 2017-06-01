631504bb03cc26a21e8c26e25d070d15bc3be62b
ThetHlaing <th.ucsy@gmail.com>
2017-06-02 00:26:45 +0800

    Cat Levels

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
 var ViewModel = function(){
     this.clickCount = ko.observable(0);
-    this.name = ko.observable("Teddy");
+    this.firstName = ko.observable("Teddy");
+    this.lastName = ko.observable("Bear");
     this.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg")
     this.imgAttribution = ko.observable("Thet Hlaing")
     this.incrementCounter = function(){
       this.clickCount(this.clickCount() + 1);
     };
+    this.fullName = ko.computed(function(){
+        return this.firstName() + " " + this.lastName();
+    },this);
+    this.catLevel = ko.computed(function(){
+      if(this.clickCount() < 10)
+      {
+        return "Newborn";
+      }
+      else if(this.clickCount() < 30){
+        return "Infant";
+      }
+      else{
+        return "Teen";
+      }
+    },this)
 }
 
 ko.applyBindings(new ViewModel);
